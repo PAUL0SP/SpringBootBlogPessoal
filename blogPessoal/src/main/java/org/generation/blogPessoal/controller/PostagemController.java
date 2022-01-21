15b5f86a432c4170cbdc4ce0a8aa79aae838ba1b
unknown <paulo.dm9@gmail.com>
2022-01-20 22:23:32 -0300

    correção de erros

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -4,12 +4,9 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.generation.blogPessoal.model.Postagem;
-import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,23 +17,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/postagens")
-@CrossOrigin("*")
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.PostagemRepository;
+
+
+
+
+@RestController                                    /*--->Indica que a classe irá responder pelos métodos http.*/
+@RequestMapping("/postagens")                      /*---> End point das requisições feitas pela URL.*/
+@CrossOrigin(origins = "*", allowedHeaders = "*")  /*--->Esta anotação, além de liberar as origens, libera também os cabeçalhos das requisições*/
 public class PostagemController {
-	@Autowired
-	private PostagemRepository repository;
+	
+	@Autowired                                     /*Injeção de independência, forma um objeto de classe controladora como inversão de controle */
+	private PostagemRepository postagemrepository;
   
   @GetMapping
   public ResponseEntity<List<Postagem>> Getall(){
 	  
-	  return ResponseEntity.ok(repository.findAll());
+	  return ResponseEntity.ok(postagemrepository.findAll()); //retornará cód 200 OK.
   }
   
   @GetMapping("/{id}")
   public ResponseEntity<Postagem> GetById(@PathVariable long id){
-	  
-	  return repository.findById(id)
+	
+	  return postagemrepository.findById(id)
 			  .map(resp -> ResponseEntity.ok(resp))
 			  .orElse(ResponseEntity.notFound().build());
 	     
@@ -45,25 +49,33 @@ public class PostagemController {
   @GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
 		
-	  return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	  return ResponseEntity.ok(postagemrepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
   
   @PostMapping
   public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem){
 	  
-	  return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	  return ResponseEntity.status(HttpStatus.CREATED).body(postagemrepository.save(postagem));
   }
   
   @PutMapping
 	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
 		
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+		return postagemrepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.ok().body(postagemrepository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
   
   }
   
   @DeleteMapping("/{id}")
-  public void delete(@PathVariable long id) {
-	  repository.deleteById(id);
+  public ResponseEntity<?> deletePostagem(@PathVariable long id) {
+		
+		return postagemrepository.findById(id)
+				.map(resposta -> {
+					postagemrepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
   }
 }
 
